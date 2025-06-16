3aa31277a6a7225cffad95841929822cb0cbf55d
Nayarr <rayan.oughlis@etu.u-pec.fr>
2025-06-16 11:37:19 +0200

    fonction get mesure

--- models.py
+++ models.py
@@ -15,33 +15,6 @@ def init_db():
         "regions": db["regions"]
     }
 
-    
-def afficher_stations(page=1, per_page=20):
-    collections = init_db()
-    stations_collection = collections["stations"]
-    stations_list = stations_collection.find()
-    
-    # Pagination MongoDB
-    skip = (page - 1) * per_page
-    stations_list = stations_collection.find().skip(skip).limit(per_page)
-
-
-    stations_data = []
-    for station in stations_list:
-        code_station = station.get("_id")  # ou la bonne clé
-        mesure = get_mesures_station(code_station)
-        m = mesure[0]  # maintenant m est un dictionnaire
-        stations_data.append({
-            "nom": station.get("libelle_station"),
-            "dpt": station.get("code_departement"),
-            "code": code_station,
-            "site": station.get("libelle_site"),
-            "commune": station.get("libelle_commune"),
-            "grandeur": m.get("grandeur_hydro_elab"),
-            "valeur": m.get("resultat_obs_elab"),
-            "date": m.get("date_obs_elab")
-        })
-    return stations_data
 
 def get_mesures_station(code_station):
     url = f"https://hubeau.eaufrance.fr/api/v2/hydrometrie/obs_elab"
