d7642cf3205fbd9e3be87d9b6f876dcda9d8bf8c
Nayarr <rayan.oughlis@etu.u-pec.fr>
2025-06-16 10:52:05 +0200

    controller

--- controller.py
+++ controller.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+import requests
+from pymongo import MongoClient
+from models import *
 
 app = Flask(__name__)
-@app.route('/')
-def index():
 
-    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def index():
+    page = int(request.args.get('page', 1))
+    stations_data = afficher_stations(page=page, per_page=20)
+    return render_template("index.html", stations=stations_data, page=page)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
